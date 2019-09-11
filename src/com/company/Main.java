2e2049559278e4496b078c0671c4c93672dec882
Cosmin Razvan <razvancluj88@yahoo.com>
2019-09-11 20:50:49 +0300

    UPP

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hellow World");
+        // Tipuri de date
+        String rasa = "Ciobanesc German"
+        String culoare = "Maro"
+        byte varsta = 2;
+        Float greutate = 23.5f
+        char gen = 'f'
+        int nivelInteligenta = 12312321234
+        long nivelEnegrie = 1234569845
+        double nivelNevoieAtentie = 12488558886
+
+        System.out.println("Despre Catelul Meu");
+        System.setOut("culoare este"+
+                culoare +
+                ", \nvarsta este "+
+                );
+    }
+}
