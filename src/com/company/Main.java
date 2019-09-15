46bed070be9b5608498ba5b3daa9887e2fef849d
Cosmin Razvan <razvancluj88@yahoo.com>
2019-09-15 11:17:15 +0300

    UPP

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,40 @@
 package com.company;
 
+import org.graalvm.compiler.debug.TTY;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hellow World");
         // Tipuri de date
-        String rasa = "Ciobanesc German"
-        String culoare = "Maro"
+        String rasa = "Ciobanesc German";
+        String culoare = "Maro";
         byte varsta = 2;
-        Float greutate = 23.5f
-        char gen = 'f'
-        int nivelInteligenta = 12312321234
-        long nivelEnegrie = 1234569845
-        double nivelNevoieAtentie = 12488558886
+        Float greutate = 23.5f;
+        char gen = 'f';
+        int nivelInteligenta = 1231232123;
+        long nivelEnegrie = 12345698454673189L;
+        double nivelNevoieAtentie = 12488558886465654745745676576583254646465430242042d;
 
         System.out.println("Despre Catelul Meu");
         System.setOut("culoare este"+
                 culoare +
                 ", \nvarsta este "+
-                );
+                varsta +
+                "si greutatea este "+
+                greutate);
+        System.out.println("Despre Catelul Meu");
+        System.out.println("rasa");
+        System.out.println("nivelNevoieAtentie");
+                System.out.println("culoare + varsta + greutate");
+
+
+                int x = 2
+                        int y = 3
+                                x+y;
+                float avrage = sum/2;
+                System.out.println(avrage);
+        System.out.println("numbers:"+ x + y)
+
     }
 }
