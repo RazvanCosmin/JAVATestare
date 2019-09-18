a77b84da6e67ca83e179640001fc15f37e9f868a
Cosmin Razvan <razvancluj88@yahoo.com>
2019-09-18 20:50:59 +0300

    upp2

---
+++ src/lab2/Animalrescue/AppMain.java
@@ -0,0 +1,10 @@
+package lab2.Animalrescue;
+
+public class AppMain {
+    public static void main (String [] args){
+
+        Cat cainepisica = new Cat ();
+        System.out.println(cainepisica.name);
+        cainepisica
+    }
+}
---
+++ src/lab2/Animalrescue/Cat.java
@@ -0,0 +1,26 @@
+package lab2.Animalrescue;
+
+import javax.print.attribute.standard.JobImpressions;
+
+public class Cat {
+    String color = "Black";
+    String breed = "Cat Dog";
+    float weight = 25f;
+    float age = 10f;
+    char gender = 'm';
+    String name = "cainepisica";
+
+    public void bark (){
+        System.out.println("WoooofMiauuuuuu");
+
+    }
+
+    public  void run (){
+        System.out.println("Running");
+    }
+
+    public void  jump (){
+        System.out.println("jump");
+
+    }
+}
