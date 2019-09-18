b62f9f367ed0f5b7869c4446f8bc88e4e65d7441
Cosmin Razvan <razvancluj88@yahoo.com>
2019-09-18 17:50:15 +0300

    upp2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import org.graalvm.compiler.debug.TTY;
 
 public class Main {
 
@@ -17,60 +16,51 @@ public class Main {
         double nivelNevoieAtentie = 12488558886465654745745676576583254646465430242042d;
 
         System.out.println("Despre Catelul Meu");
-        System.setOut("culoare este"+
-                culoare +
-                ", \nvarsta este "+
+        System.out.println("culoare este" +
+                "culoare" +
+                ", \nvarsta este " +
                 varsta +
-                "si greutatea este "+
+                "si greutatea este " +
                 greutate);
         System.out.println("Despre Catelul Meu");
         System.out.println("rasa");
         System.out.println("nivelNevoieAtentie");
-                System.out.println("culoare + varsta + greutate");
+        System.out.println("culoare + varsta + greutate");
 
 
-                int x = 2;
-                        int y = 3;
-                                x+y;
-                float avrage = x + y;
-                System.out.println(avrage);
-        System.out.println("numbers:"+ x + y)
+        int x = 2;
+        int y = 3;
+        int sum = x + y;
+        float avrage = x + y;
+        System.out.println(avrage);
+        System.out.println("numbers:" + x + y);
 
-    }
-}
-// Coment
-package com.company;
-import org.graalvm.compiler.debug.TTY;
-public class Main {
 
-    public static void main(String[] args) {
-
-        System.out.println("Hellow Razvan); "
+        System.out.println("Hellow Razvan");
                 // Rase De Caini
 
-                String rasa = "Amstaff";
-        String culoare = "Maro";
-        byte varsta = 8;
-        Float greutate = 20.5f;
-        char gen = 'f';
-        int nivelInteligenta = 1231232123;
-        long nivelEnegrie = 12345698454673189L;
-        double nivelNevoieAtentie = 12488558886465654745745676576583254646465430242042d
-        System.out.println("Despre Catelul Meu");
-
-        int x = -5;
-                int y = 8*6;
-                        float avrage = -x + y;
-                                System.out.println("numbers:"-x + y);
-
-
-                               int x = 55;
-                                int x1 = 9;
-                                int y = 9;
-                                float avrage = x + x1;
-
-                                System.setOut("avrage + y");
-
+//                String rasa = "Amstaff";
+//        String culoare = "Maro";
+//        byte varsta = 8;
+//        Float greutate = 20.5f;
+//        char gen = 'f';
+//        int nivelInteligenta = 1231232123;
+//        long nivelEnegrie = 12345698454673189L;
+//        double nivelNevoieAtentie = 12488558886465654745745676576583254646465430242042d
+//        System.out.println("Despre Catelul Meu");
+//
+//        int x = -5;
+//        int y = 8 * 6;
+//        float avrage = -x + y;
+//        System.out.println("numbers:" - x + y);
+//
+//
+//        int x = 55;
+//        int x1 = 9;
+//        int y = 9;
+//        float avrage = x + x1;
+//
+//        System.setOut("avrage + y");
 
     }
 }
