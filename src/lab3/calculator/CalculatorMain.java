4574f56e62708dd430b6f7b842dd202afc3ff9dc
Cosmin Razvan <razvancluj88@yahoo.com>
2019-10-09 20:43:19 +0300

    upp2

--- src/lab3/calculator/CalculatorMain.java
+++ src/lab3/calculator/CalculatorMain.java
@@ -1,4 +1,17 @@
 package lab3.calculator;
 
 public class CalculatorMain {
+
+    public static void main(String[] args) {
+
+
+        String text = "FastTrackj";
+        int number = 5;
+        int y = 4;
+        LogicalOp op = new LogicalOp();
+        String textf = "FastTrack";
+        int numberh = 8;
+        String result = op.verifyTextAndNumber(text , number);
+        System.out.println(result);
+    }
 }
--- src/lab3/calculator/LogicalOp.java
+++ src/lab3/calculator/LogicalOp.java
@@ -1,6 +1,16 @@
 package lab3.calculator;
 
-public class CalculatorMain {
-    public int verifyTextAndNumber (int first, int second);
+public class LogicalOp {
 
+    public String verifyTextAndNumber(String text, int number) {
+        if (text.equals("FastTrack.") && number <= 3) {
+            return (text + "  " + number);
+        } else if (!text.equals("FastTrack") && number >=4)
+            return(number + "   "+ text);
+
+        return "";
+    }
 }
+
+
+
