41451b88342793652b4fc712e5f056903cb1e216
Cosmin Razvan <razvancluj88@yahoo.com>
2019-10-09 19:53:23 +0300

    upp2

---
+++ src/lab3/calculator/LogicalOp.java
@@ -0,0 +1,6 @@
+package lab3.calculator;
+
+public class LogicalOp {
+    public int verifyGreaterNumber(int first, int second);
+        if
+}
---
+++ src/lab3/calculator/Main.java
@@ -0,0 +1,5 @@
+package lab3.calculator;
+
+public class LogicalOp{
+    public int verifyTextAndNumber(String text, int number);
+}
