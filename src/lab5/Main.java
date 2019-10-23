4e05e5b9f99f615dc9b2244959ba447ec0bcbef6
Cosmin Razvan <razvancluj88@yahoo.com>
2019-10-23 20:01:33 +0300

    upp2

---
+++ src/lab5/Main.java
@@ -0,0 +1,10 @@
+package lab5;
+
+public class Main {
+    public static void main (String[] args ){
+        int[] myArry = new int [100];
+        myArry[0] = 1;
+        myArry[1] = 2;
+        for (int i = )
+    }
+}
