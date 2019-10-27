c200c3ef503b4c0a172a96b0aab1bf19254293b7
Cosmin Razvan <razvancluj88@yahoo.com>
2019-10-27 16:18:05 +0200

    upp2

--- src/lab5/Main.java
+++ src/lab5/Main.java
@@ -5,6 +5,15 @@ public class Main {
         int[] myArry = new int [100];
         myArry[0] = 1;
         myArry[1] = 2;
-        for (int i = )
+
+            public static void main(String[] args) {
+            LogicalOp op = new LogicalOp();
+            int[] newArray = op.getArrayHundred();
+            int[] anotherOne = {1,2,3,4,5};
+            float[] x = new float[3];
+            x[0]=1;
+
+
     }
+
 }
