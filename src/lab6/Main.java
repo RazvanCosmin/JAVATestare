99c5a0415ca6ee2873f27358d27d776685ad76db
Cosmin Razvan <razvancluj88@yahoo.com>
2019-10-30 19:25:42 +0200

    upp2

---
+++ src/lab6/Main.java
@@ -0,0 +1,18 @@
+package lab6;
+
+import java.util.*;
+
+public class Main {
+    public static void main (String[] aegs) {
+        List<Integer> myList = new ArrayList<>();
+
+        myList.add(1);
+        myList.add(3);
+        myList.add(2);
+        myList.add(5);
+
+        myList.remove(Integer.valueOf(3));
+        System.out.println(myList);
+        System.out.println(myList.contains();
+    }
+    }
