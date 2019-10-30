e9350bb070b21ab5dc2a708ffcbbc3a97adcb783
Cosmin Razvan <razvancluj88@yahoo.com>
2019-10-30 18:22:18 +0200

    upp2

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -7,64 +7,52 @@ public class Main {
 
         int x = 3;
         int y = 5;
-        float result = sum(x,y);
+        float result = sum(x, y);
         System.out.println(result);
-        result = division (x,y);
+        result = division(x, y);
         System.out.println(result);
-        result = avrage (x,y);
+        result = avrage(x, y);
         System.out.println(result);
-        result = substract(x,y)
-                System.out.println(result);
+        result = substract(x, y);
+        System.out.println(result);
     }
 
-    public static int sum (int first, int second ){
+    public static int sum(int first, int second) {
         int sum = first + second;
-                return sum;
+        return sum;
 
     }
-    public static int avrage (int first , int second){
+
+    public static int avrage(int first, int second) {
         int avrage = first * second;
-                return avrage;
+        return avrage;
 
     }
-    public static float division (float first , float second){
+
+    public static float division(float first, float second) {
         float division = first / second;
-                return division;
+        return division;
     }
-    public static int substract (int first, int second){
+
+    public static int substract(int first, int second) {
         int substract = first + second;
-                return substract;
+        return substract;
     }
-    public class Main {
 
-        public static void main(String[] args) {
 
-            printMyName();
+    public static void printMyName() {
 
-        }
-
-        public static void printMyName(){
-
-            System.out.println("Hello \n Cosmin");
-
-        }
+        System.out.println("Hello \n Cosmin");
 
     }
-    public class Main {
 
-        public static void main(String[] args) {
 
-            System.out.println("Rezultatul adunarii este: " + adunare(2,3));
+    public static int adunare(int primulNumar, int alDoileaNumar) {
 
-        }
+        int rezultat = primulNumar + alDoileaNumar;
 
-        public static int adunare(int primulNumar, int alDoileaNumar){
-
-            int rezultat = primulNumar + alDoileaNumar;
-
-            return rezultat;
-
-        }
+        return rezultat;
 
     }
+
 }
