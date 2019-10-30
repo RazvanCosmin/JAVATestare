99a0d1818f2accabc5a6ff5a374b4fdfac1cd97d
Cosmin Razvan <razvancluj88@yahoo.com>
2019-10-30 19:40:35 +0200

    upp2

--- src/lab6/Calculator/LogicalOp.java
+++ src/lab6/Calculator/LogicalOp.java
@@ -1,5 +1,19 @@
 package lab6.Calculator;
 
+import java.util.List;
+
 public class LogicalOp {
-    public void addToList(List<Integer>)
-}
+
+
+
+        //    Scrieti o metoda Java, care sa primeasca doi parametrii:
+//    un parametru sa fie o lista de numere, si celalalt un numar
+//    (real sau intreg). Metoda sa adauge numarul primit ca si parametru
+//    la final de lista.
+        public void addToList(List<Integer> list, int number){
+            list.add(number);
+        }
+
+    }
+
+
