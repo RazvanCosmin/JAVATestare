fa4cb06abebd64df6c65890ad2dc6308aa67f5b0
Cosmin Razvan <razvancluj88@yahoo.com>
2019-11-04 19:41:03 +0200

    upp2

---
+++ src/lab6/AnimalRescue/AmericanStaffordshireTerrier.java
@@ -0,0 +1,29 @@
+package lab6.AnimalRescue;{
+
+public class AmericanStaffordshireTerrier extends Dog {
+    @Override
+    public void eat() {
+        System.out.println( eat("Roial Canin");
+
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(speak("Arr Ham Ham Ham Ham" ));
+    }
+
+
+    @Override
+    public void Train () {
+        System.out.println(Train(1,2,3,4,5,6,));
+    }
+
+
+    @Override
+    public void Attack () {
+        System.out.println(Attack(Attack(Attack(););););
+    }
+}
+}
+
+
