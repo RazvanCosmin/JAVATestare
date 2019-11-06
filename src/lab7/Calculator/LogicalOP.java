4233b00a816c7c02f0824b5129d4cd54b6f46071
Cosmin Razvan <razvancluj88@yahoo.com>
2019-11-06 19:37:40 +0200

    upp2

---
+++ src/lab7/Calculator/LogicalOP.java
@@ -0,0 +1,18 @@
+package lab7.Calculator;
+
+import java.util.List;
+
+public class LogicalOP {
+    public void printListValuesReversed(List<Integer> myList){
+
+        for (int i = myList.size()-1; i >= 0; i--){
+            System.out.println(myList.get(i));
+        }
+
+    }
+
+
+
+
+}
+
